dbf658a1bf5d50df13a758c2818e717112592087
pw-tf <kxania.a2p@gmail.com>
2026-03-16 10:29:18 +0930

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -2,4 +2,4 @@
 const SUPABASE_URL = 'https://nlkcncnzlaunxvuklgpr.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5sa2NuY256bGF1bnh2dWtsZ3ByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NTYwMTgsImV4cCI6MjA4OTEzMjAxOH0.UH2ucbFNkOw_iam3n-9bmT_LXvYJZrR09978pgzhOYw';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
